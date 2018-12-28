f30f4ebcd77db1d31c8a7cc921cec8d82e05f01d
Zhang Xuyan <377852592@qq.com>
2018-12-28 17:51:57 +0800

    miss the application.properties' password

--- Lession 7/src/main/java/com/nowcoder/WendaApplication.java
+++ Lession 7/src/main/java/com/nowcoder/WendaApplication.java
@@ -11,3 +11,8 @@ public class WendaApplication {
 		SpringApplication.run(WendaApplication.class, args);
 	}
 }
+
+// 最后一点：
+// ****************************
+// 终于知道知道Lession 7死活没有对request的响应了，我换了Lession 8中的全部静态文件（resources）到Lession 7中发现可以了
+// *****************************
